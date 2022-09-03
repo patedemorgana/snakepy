7eda4da6fb61d8c13b3decd7d109139c45a7aba2
patedemorgana <gabriel.volpate@gmail.com>
2022-09-03 14:00:44 -0300

    sdasd

--- functions.py
+++ functions.py
@@ -2,12 +2,55 @@ import pygame
 
 def board():
     pygame.init()
-    dis=pygame.display.set_mode((400,300))
+
+    width = 1500
+    height = 800
+
+    white=(255, 255, 255)
+    black=(0,0,0)
+    red=(255,0,0)
+
+    x1 = width/2
+    y1 = height/2 - 10
+
+    x1_mudando = 0
+    y1_mudando = 0
+
+    clock = pygame.time.Clock()
+
+    board=pygame.display.set_mode((width,height))
+    board.fill((255, 255, 255))
+
     pygame.display.update()
     pygame.display.set_caption('snak')
+    
     perdeu=False
+
     while not perdeu:
         for event in pygame.event.get():
+
+            if event.type == pygame.QUIT:
+                perdeu = True
+
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    x1_mudando = -10
+                    y1_mudando = 0
+                if event.key == pygame.K_RIGHT:
+                    x1_mudando = 10
+                    y1_mudando = 0
+                if event.key == pygame.K_UP:
+                    x1_mudando = 0
+                    y1_mudando = -10
+                if event.key == pygame.K_DOWN:
+                    x1_mudando = 0
+                    y1_mudando = 10
+
             print(event)
+        x1 += x1_mudando
+        y1 += y1_mudando
+        pygame.draw.circle(board, black, [x1, y1], 10, 0)
+        pygame.display.update()
+        clock.tick(30)
     pygame.quit()        
     quit()
